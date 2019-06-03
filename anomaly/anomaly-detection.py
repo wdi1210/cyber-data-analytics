52b3b244313af5b800b7848d2d54aab437d7bb1c
Wouter Zirkzee <W.J.Zirkzee@student.tudelft.nl>
2019-06-03 14:47:43 +0200

    basic pytorch setup

--- anomaly/anomaly-detection.py
+++ anomaly/anomaly-detection.py
@@ -6,14 +6,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import os
+import math
 
-DATAPATH = 'data/'
+
+DATAPATH = 'anomaly/data'
 
 dateparse = lambda x: pd.datetime.strptime(x, '%d/%m/%y %H')
 
-data3 = pd.read_csv(DATAPATH +'BATADAL_dataset03.csv', index_col=0, parse_dates=[0], date_parser=dateparse)
-data4 = pd.read_csv(DATAPATH +'BATADAL_dataset04.csv', index_col=0, parse_dates=[0], date_parser=dateparse)
-data_test = pd.read_csv(DATAPATH +'BATADAL_test_dataset.csv', index_col=0, parse_dates=[0], date_parser=dateparse)
+data3 = pd.read_csv(os.path.join(DATAPATH, 'BATADAL_dataset03.csv'), index_col=0, 
+parse_dates=[0], date_parser=dateparse)
+data4 = pd.read_csv(os.path.join(DATAPATH, 'BATADAL_dataset04.csv'), index_col=0, parse_dates=[0], date_parser=dateparse)
+data_test = pd.read_csv(os.path.join(DATAPATH, 'BATADAL_test_dataset.csv'), index_col=0, parse_dates=[0], date_parser=dateparse)
 
 # data4 and data_test seem to have spaces before the labels....
 data4.columns = data3.columns
@@ -78,7 +82,7 @@ data4.drop(columns=['ATT_FLAG'], inplace=True)
 # In[]:
 from statsmodels.tsa.ar_model import AR
 
-data = data3 = pd.read_csv(DATAPATH +'BATADAL_dataset03.csv')
+data = data3
 
 model = AR(getTankLevel(data, 1)).fit()
 yhat = model.predict(len(data)-30, len(data))
@@ -100,9 +104,6 @@ plt.show()
 from statsmodels.tsa.arima_model import ARMA
 from pandas.plotting import autocorrelation_plot
 
-data3 = pd.read_csv(DATAPATH + 'BATADAL_dataset03.csv')
-data4 = pd.read_csv(DATAPATH + 'BATADAL_dataset04.csv')
-data4.columns = data3.columns
 data = data4['L_T1'] # Update accordingly between L_T1 - L_T5
 
 test_train_split = int(len(data) * 0.9)
@@ -182,7 +183,6 @@ plt.savefig("discretization-group.png")
 sns_plot = sns.lmplot(x='DATETIME', y='L_T1', hue='label', data=dataDisc.reset_index(), fit_reg=False)
 plt.savefig("discretization-label.png")
 
-<<<<<<< HEAD
 # print(''.join(dataDisc['group']))
 
 # from prefixspan import PrefixSpan
@@ -190,8 +190,6 @@ plt.savefig("discretization-label.png")
 
 # print(ps.frequent(7))
 
-=======
->>>>>>> 443209ca29e79883219e8a08c70f9fa6787d8447
 # cut_data.head()
 # time = getDate(data4)
 # plt.bar(data4), data)
@@ -297,6 +295,122 @@ PCArecall = len(TP.index) / ( len(TP.index) + len(FN.index))
 print(PCAprecision, PCArecall)
 
 
+
+######################
+##### Bonus: PyTorch
+######################
+
+# Define Pytorch long short term memory network
+# In[]:
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+class LSTM(nn.Module):
+
+    def __init__(self, input_dim, hidden_dim, batch_size, output_dim=1,
+                    num_layers=2):
+        super(LSTM, self).__init__()
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.batch_size = batch_size
+        self.num_layers = num_layers
+
+        # LSTM layer
+        self.lstm = nn.LSTM(self.input_dim, self.hidden_dim, self.num_layers)
+        # Output layer
+        self.linear = nn.Linear(self.hidden_dim, output_dim)
+        # Loss function
+        self.loss_fn = nn.MSELoss()
+
+    # Initial hidden states
+    def init_hidden(self):
+        return (torch.zeros(self.num_layers, self.batch_size, self.hidden_dim),
+                torch.zeros(self.num_layers, self.batch_size, self.hidden_dim))
+
+    # Forward pass and return the prediction (single step only)
+    def forward(self, input):
+        lstm_out, self.hidden = self.lstm(input.view(len(input), self.batch_size, -1))
+        # prediction
+        y_pred = self.linear(lstm_out[-1].view(self.batch_size, -1))
+        return y_pred.view(-1)
+
+    # Compute loss
+    def loss(self, pred, truth):
+        return self.loss_fn(pred, truth)
+
+
+
+# Define and train model
+# In[]
+
+def timerseries_test_train_split(data, labels, split):
+    split_Nr = int(len(data) * split)
+    data = data.as_matrix()
+    labels = labels.as_matrix()
+    X_train = data[:-split_Nr]
+    X_test = data[-split_Nr]
+    y_train = labels[:-split_Nr]
+    y_test = labels[-split_Nr]
+
+    return [X_train, X_test, y_train, y_test]
+    
+test_size = 0.2
+torch_data = getTankLevel(data3, 1)
+X_train, X_test, y_train, y_test = timerseries_test_train_split(torch_data, data3_labels, test_size)
+
+
+# Data split
+X_train = torch.from_numpy(X_train).type(torch.Tensor).view([1, -1, 1])
+# X_test = torch.from_numpy(stable_ar.X_test).type(torch.Tensor).view([input_size, -1, 1])
+y_train = torch.from_numpy(y_train).type(torch.Tensor).view(-1)
+# y_test = torch.from_numpy(stable_ar.y_test).type(torch.Tensor)
+
+
+# Define model and optimiser
+model = LSTM(input_dim=1, hidden_dim=32, batch_size=math.ceil((1-test_size)*len(torch_data)), output_dim=1, num_layers=2)
+training_epochs = 100
+optimiser = torch.optim.Adam(model.parameters(), lr=0.001)
+
+# set training mode
+model.train()
+# run epochs
+hist = np.zeros(training_epochs)
+for t in range(training_epochs):
+    # Clear stored gradient
+    model.zero_grad()
+    
+    # Initialise hidden state
+    # Don't do this if you want your LSTM to be stateful
+    model.hidden = model.init_hidden()
+    
+    # Forward pass
+    y_pred = model.forward(X_train)
+
+    # Compute loss
+    loss = model.loss(y_pred, y_train)
+    hist[t] = loss.item()
+
+
+    # Zero out gradient between steps
+    optimiser.zero_grad()
+    # Backward pass
+    loss.backward()
+    # Update parameters
+    optimiser.step()
+
+# Plots and performance
+
+print(y_train)
+plt.plot(y_pred.detach().numpy(), label="Predictions")
+plt.plot(y_train.detach().numpy(), label="Data")
+plt.legend()
+plt.show()
+
+plt.plot(hist, label="Training loss")
+plt.legend()
+plt.show()
 
 
 #%%
