71c974f6de67c9233d7ab660f1383f15aeb6e84a
Wouter Zirkzee <W.J.Zirkzee@student.tudelft.nl>
2019-06-15 14:51:56 +0200

    countminsketch from yesterday

--- lab3/lab.py
+++ lab3/lab.py
@@ -4,7 +4,12 @@
 ################################
 import os
 import pandas as pd
+import numpy as np
+import mmh3
+import random
+import matplotlib.pyplot as plt
 
+#%%
 DATAFILE = r'lab3/data/capture20110812.pcap.netflow.labeled.csv'
 
 col_names = ['Date_1', 'Date_2', 'Durat', 'Prot', 'SrcIPAddr:Port', 'direction', 'DstAddr:Port.1', 'Flags', 'Tos', 'Packets', 'Bytes',
@@ -15,9 +20,6 @@ csv = pd.read_csv(DATAFILE, delimiter='\s+', skiprows=1, names=col_names)
 # In[]
 # Util functions
 ###############################
-import pandas as pd
-import random
-
 def modifyData(df):
     df['Date'] = df['Date_1'] + ' ' + df['Date_2']
     df.set_index('Date', inplace=True)
@@ -53,7 +55,30 @@ sample['SrcIPAddr:Port'].value_counts()
 ##############################
 # Sketching Task
 ##############################
+class CountMinSketch():
 
+    def __init__(self, width, depth, hash_function):        
+           self.__width = width
+           self.__depth = depth
+           self._bins = np.zeros((width, depth))
+           self._hash_function = hash_function
+
+    def add(self, key):
+        values = list()
+        for i in range(self.__depth):
+            val = self._hash_function(key, seed=i, signed=False) % self.__width
+            self._bins[val ][i] += 1
+            values.append(val)
+        return values
+
+cms = CountMinSketch(1000, 10, mmh3.hash)
+
+for index, row in csv.head(100000).iterrows():
+    if (index % 10000) == 0:
+        print('{}/{}'.format(index, len(csv), end='\r'))
+    cms.add(row['SrcIPAddr:Port'])
+#%%
+display(cms._bins)
 
 #%%
 ###############################
